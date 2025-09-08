abb01855d7a8bd71072a52f6eaa3dbda433b0e9b
Satish More <111186122+SatishM35@users.noreply.github.com>
2025-09-08 15:50:08 +0530

    API worked

--- src/app/service/service.ts
+++ src/app/service/service.ts
@@ -10,14 +10,15 @@ export class Service {
   
   
   private apiKey = '058910a23841670548d7dd08231fc114'; // Replace with your actual API key
-  private baseUrl = 'https://api.openweathermap.org/data/3.0/weather'; // Example using OpenWeatherMap
+  private baseUrl = 'https://api.openweathermap.org/data/2.5/weather'; // Example using OpenWeatherMap
 
   
   constructor(private http: HttpClient) {}
 
   getWeatherData(city: string): Observable<any> {
     const url = `${this.baseUrl}?q=${city}&appid=${this.apiKey}`; // Add units for Celsius
-    return this.http.get(url);
+    const url1 = `https://api.openweathermap.org/data/2.5/weather?q=mumbai&appid=058910a23841670548d7dd08231fc114`;
+    return this.http.get(url1);
   }
 
 }
